25fc36f124adf867f3463b578fd88c3296366fa2
Wiktor Bandura <wiktor@MBP-Wiktor.home>
2022-09-11 17:07:14 +0200

    JavaScript functionality for readmore modal on section memories

---
+++ js/modal.js
@@ -0,0 +1,55 @@
+const memoryBoxes = [...document.querySelectorAll('.section-memories__memories .memory')];
+const body = document.querySelector('body');
+
+const generateModal = (memory) => {
+    const modalTitle = document.createElement('h4');
+    const modalAuthorName = document.createElement('h5');
+    const modalContent = document.createElement('p');
+    const modal = document.createElement('div');
+    const modalClose = document.createElement('span');
+
+    modalTitle.textContent = memory.title;
+    modalAuthorName.textContent = memory.name;
+    modalContent.textContent = memory.content;
+    modalClose.innerHTML = `&#10005;`;
+
+    modalTitle.classList.add('modal__title', 'heading', 'heading-4', 'heading--primary');
+    modalAuthorName.classList.add('modal__author-name');
+    modalContent.classList.add('modal__content');
+    modalClose.classList.add('modal__close');
+    modal.classList.add('modal');
+
+    modal.append(
+        modalTitle,
+        modalAuthorName,
+        modalContent,
+        modalClose
+    );
+
+    body.appendChild(modal);
+
+    modalClose.addEventListener('click', () => {
+        body.removeChild(modal);
+    })
+}
+
+const showErrorMessage = (error) => alert('Ups! Coś poszło nie tak! Spróbuj ponownie później', error);
+
+const getModalsContent = (clickedMemoryTitle) => {
+    fetch('./memories.json')
+    .then((response) => response.json())
+    .then((data) => {
+        const found = data.memories.find(memory => memory.title === clickedMemoryTitle);
+
+        generateModal(found);
+
+    })
+    .catch((error) => {
+        console.error(error);
+        showErrorMessage(error);
+    });
+}
+
+memoryBoxes.forEach(memoryBox => memoryBox.addEventListener('click', () => {
+    getModalsContent(memoryBox.dataset.title);
+}));
